8c02e75ba340046e88109d69e97cd57aca95e00d
Bianca <urbisagliabianca@gmail.com>
2023-08-10 15:36:22 -0300

    Cambio de planes

---
+++ js/clases.js
@@ -0,0 +1,17 @@
+class Productos {
+	constructor(nombre,id,precio,stock){
+		this.nombre = nombre;
+		this.id = id;
+		this.precio = precio;
+		this.stock = stock;
+	}
+}
+
+class Usuario {
+	constructor(nombre,correo,telefono,compra){
+		this.nombre = nombre;
+		this.correo = correo;
+		this.telefono = telefono;
+		this.compra = compra;
+	}
+}
--- js/script.js
+++ js/script.js
@@ -1,17 +1,146 @@
-class Productos {
-	constructor(nombre,codigo,precio,stock){
-		this.nombre = nombre;
-		this.codigo = codigo;
-		this.precio = precio;
-		this.stock = stock;
-	}
-}
 
-class Usuario {
-	constructor(nombre,correo,telefono,compra){
-		this.nombre = nombre;
-		this.correo = correo;
-		this.telefono = telefono;
-		this.compra = compra;
+
+/* *********PRODUCTOS ******************/
+
+const productosTienda = [
+	{nombre:'Auriculares', id:'AUR01', precio: 30000, stock:5},
+	{nombre:'Reloj', id:'R01', precio: 35000, stock:6},
+	{nombre:'Smartwatch', id:'SM01', precio: 60000, stock:6},
+	{nombre:'Notebook', id:'NOT01', precio: 120000, stock:3},
+	{nombre:'Teclado', id:'TCL01', precio: 25000, stock:7},
+	{nombre:'Mouse', id:'MOU01', precio: 10000, stock:10},
+	{nombre:'Micrófono', id:'MICL01', precio: 15000, stock:7},
+	{nombre:'Auriculares inalambricos', id:'AUR02', precio: 20000, stock:9},
+	{nombre:'Tablet', id:'TAB01', precio: 85000, stock:6},
+	{nombre:'Cargador inalambrico', id:'CAR01', precio: 50000, stock:12},
+	
+	
+]
+
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
+
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
+});
+
+/* ========================= */
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector('.container-items');
+
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
+
+productsList.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
+
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
+
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
+
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
+
+		showHTML();
 	}
-}
+});
+
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
+
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
+
+		console.log(allProducts);
+
+		showHTML();
+	}
+});
+
+// Funcion para mostrar  HTML
+const showHTML = () => {
+	if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
+
+	// Limpiar HTML
+	rowProduct.innerHTML = '';
+
+	let total = 0;
+	let totalOfProducts = 0;
+
+	allProducts.forEach(product => {
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
+
+		containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+		rowProduct.append(containerProduct);
+
+		total =
+			total + parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+	});
+
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
+};
