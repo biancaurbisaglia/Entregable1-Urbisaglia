0f31dd7e272ae82164044ee3ce34da58ef186be6
Bianca <urbisagliabianca@gmail.com>
2023-08-10 18:24:53 -0300

    Cambio definitivo del navbar y cambio url de las imagenes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,92 @@
 
 
+/* *********CLASES ******************/
+
+/* class Producto {
+	constructor(nombre,id,precio,stock){
+		this.nombre = nombre;
+		this.id = id;
+		this.precio = precio;
+		this.stock = stock;
+	}
+}
+
+class Usuario {
+	constructor(nombre,correo,telefono,compra){
+		this.nombre = nombre;
+		this.correo = correo;
+		this.telefono = telefono;
+		this.compra = compra;
+	}
+} */
+
+
 /* *********PRODUCTOS ******************/
 
-const productosTienda = [
-	{nombre:'Auriculares', id:'AUR01', precio: 30000, stock:5},
-	{nombre:'Reloj', id:'R01', precio: 35000, stock:6},
-	{nombre:'Smartwatch', id:'SM01', precio: 60000, stock:6},
-	{nombre:'Notebook', id:'NOT01', precio: 120000, stock:3},
-	{nombre:'Teclado', id:'TCL01', precio: 25000, stock:7},
-	{nombre:'Mouse', id:'MOU01', precio: 10000, stock:10},
-	{nombre:'Micrófono', id:'MICL01', precio: 15000, stock:7},
-	{nombre:'Auriculares inalambricos', id:'AUR02', precio: 20000, stock:9},
-	{nombre:'Tablet', id:'TAB01', precio: 85000, stock:6},
-	{nombre:'Cargador inalambrico', id:'CAR01', precio: 50000, stock:12},
+const productos = [
+	{nombre:'Auriculares',id:'AUR01', precio: 30000,stock:5,img:'https://images.unsplash.com/photo-1583394838336-acd977736f90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=384&q=80',},
+	{nombre:'Reloj', id:'R01', precio: 35000, stock:6,img:'https://images.unsplash.com/photo-1616928231359-fc8b7e244c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80',},
+	{nombre:'Smartwatch', id:'SM01', precio: 60000, stock:6,img:'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',},
+	{nombre:'Notebook', id:'NOT01', precio: 120000, stock:3,img:'https://plus.unsplash.com/premium_photo-1681566925294-7ff6eba5a9c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',},
+	{nombre:'Teclado', id:'TCL01', precio: 25000, stock:7,img:'https://images.unsplash.com/photo-1541140532154-b024d705b90a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=436&q=80',},
+	{nombre:'Mouse', id:'MOU01', precio: 10000, stock:10,img:'https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=867&q=80',},
+	{nombre:'Micrófono', id:'MICL01', precio: 15000, stock:7,img:'https://images.unsplash.com/photo-1585102651425-8caf7848e44b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=581&q=80',},
+	{nombre:'Auriculares inalambricos', id:'AUR02', precio: 20000, stock:9,img:'https://images.unsplash.com/photo-1505236273191-1dce886b01e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',},
+	{nombre:'Tablet', id:'TAB01', precio: 85000, stock:8,img:'https://images.unsplash.com/photo-1589739900266-43b2843f4c12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aXBhZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',},
+	{nombre:'Cargador inalambrico', id:'CAR01', precio: 50000, stock:12,img:'https://images.unsplash.com/photo-1545235616-db3cd822ad8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80',},
 	
 	
 ]
 
-const btnCart = document.querySelector('.container-cart-icon');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const btnCart = document.querySelector('.container-cart-icon');
 const containerCartProducts = document.querySelector(
 	'.container-cart-products'
 );
@@ -27,14 +96,14 @@ btnCart.addEventListener('click', () => {
 });
 
 /* ========================= */
-const cartInfo = document.querySelector('.cart-product');
-const rowProduct = document.querySelector('.row-product');
+/* const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product'); */
 
 // Lista de todos los contenedores de productos
-const productsList = document.querySelector('.container-items');
+/* const productsList = document.querySelector('.container-items'); */
 
 // Variable de arreglos de Productos
-let allProducts = [];
+/* let allProducts = [];
 
 const valorTotal = document.querySelector('.total-pagar');
 
@@ -88,10 +157,10 @@ rowProduct.addEventListener('click', e => {
 
 		showHTML();
 	}
-});
+}); */
 
 // Funcion para mostrar  HTML
-const showHTML = () => {
+/* const showHTML = () => {
 	if (!allProducts.length) {
 		cartEmpty.classList.remove('hidden');
 		rowProduct.classList.add('hidden');
@@ -143,4 +212,4 @@ const showHTML = () => {
 
 	valorTotal.innerText = `$${total}`;
 	countProducts.innerText = totalOfProducts;
-};
+}; */ 
